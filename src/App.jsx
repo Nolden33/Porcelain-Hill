342ecd376753d0d6bd59494dd555e630cea427cc
Jonathon Nolden <jnolden3927@gmail.com>
2022-05-30 10:41:28 -0700

    removed footer folder, added top border, added a red to css

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,32 @@ import ShowDates from "./components/showDates/ShowDates";
 import Music from "./components/music/Music";
 import Photos from "./components/photos/Photos";
 import PageBanner from "./components/PageBanner";
+import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
-import Footer from "./components/footer/Footer";
 
 function App() {
-  return (
-    <React.Fragment>
-      <div className="app-bg">
-        <header>
-          <PageBanner />
-          <NavBar />
-        </header>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/bio" element={<Bio />}></Route>
-          <Route path="/showdates" element={<ShowDates />}></Route>
-          <Route path="/music" element={<Music />}></Route>
-          <Route path="/photos" element={<Photos />}></Route>
-        </Routes>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<div className="app-bg">
+				<header>
+					<PageBanner />
+					<NavBar />
+				</header>
+				<Routes>
+					<Route path="/" element={<HomePage />}></Route>
+					<Route path="/bio" element={<Bio />}></Route>
+					<Route path="/showdates" element={<ShowDates />}></Route>
+					<Route path="/music" element={<Music />}></Route>
+					<Route path="/photos" element={<Photos />}></Route>
+				</Routes>
+				<footer>
+					<div class="border-top border-white w-75%">
+						<Footer />
+					</div>
+				</footer>
+			</div>
+		</React.Fragment>
+	);
 }
 
 export default App;
